727d0dde4894b0edaad9e8bb3b10bbabf401782c
Michael Arthur <michael.arthur@propellerhead.co.nz>
2023-02-23 04:02:50 +0000

    make the entities easier to extend and add one more

--- homeassistant/components/electric_kiwi/const.py
+++ homeassistant/components/electric_kiwi/const.py
@@ -9,3 +9,7 @@ OAUTH2_TOKEN = "https://welcome.electrickiwi.co.nz/oauth/token"
 API_BASE_URL = "https://api.electrickiwi.co.nz"
 
 SCOPE_VALUES = "read_connection_detail read_billing_frequency read_account_running_balance read_consumption_summary read_consumption_averages read_hop_intervals_config read_hop_connection save_hop_connection read_session"
+
+
+ATTR_TOTAL_RUNNING_BALANCE = "total_running_balance"
+ATTR_TOTAL_CURRENT_BALANCE = "total_account_balance"
--- homeassistant/components/electric_kiwi/sensor.py
+++ homeassistant/components/electric_kiwi/sensor.py
@@ -1,4 +1,8 @@
 """Support for Electric Kiwi account balance."""
+from __future__ import annotations
+
+from collections.abc import Callable
+from dataclasses import dataclass
 from datetime import timedelta
 import logging
 
@@ -7,20 +11,56 @@ from electrickiwi_api import ElectricKiwiApi
 from electrickiwi_api.exceptions import ApiException
 from electrickiwi_api.model import AccountBalance
 
-from homeassistant.components.sensor import SensorEntity
+from homeassistant.components.sensor import SensorEntity, SensorEntityDescription
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import ATTR_ATTRIBUTION, ATTR_FRIENDLY_NAME, CURRENCY_DOLLAR
+from homeassistant.const import ATTR_ATTRIBUTION, CURRENCY_DOLLAR
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.util import Throttle
 
-from .const import ATTRIBUTION, DOMAIN, NAME
+from .const import (
+    ATTR_TOTAL_CURRENT_BALANCE,
+    ATTR_TOTAL_RUNNING_BALANCE,
+    ATTRIBUTION,
+    DOMAIN,
+    NAME,
+)
 
 _LOGGER = logging.getLogger(__name__)
 
 SCAN_INTERVAL = timedelta(hours=24)
 
-FRIENDLY_NAME = f"{NAME} Account Balance"
+
+@dataclass
+class ElectricKiwiRequiredKeysMixin:
+    """Mixin for required keys."""
+
+    value_func: Callable[[AccountBalance], int | str | None]
+
+
+@dataclass
+class ElectricKiwiSensorEntityDescription(
+    SensorEntityDescription, ElectricKiwiRequiredKeysMixin
+):
+    """Describes Electric Kiwi sensor entity."""
+
+
+SENSOR_TYPES: tuple[ElectricKiwiSensorEntityDescription, ...] = (
+    ElectricKiwiSensorEntityDescription(
+        key=ATTR_TOTAL_RUNNING_BALANCE,
+        name=f"{NAME} total running balance",
+        icon="mdi:currency-usd",
+        native_unit_of_measurement=CURRENCY_DOLLAR,
+        value_func=lambda account_balance: account_balance.total_running_balance,
+    ),
+    ElectricKiwiSensorEntityDescription(
+        key=ATTR_TOTAL_CURRENT_BALANCE,
+        name=f"{NAME} total current balance",
+        icon="mdi:currency-usd",
+        native_unit_of_measurement=CURRENCY_DOLLAR,
+        value_func=lambda account_balance: account_balance.total_account_balance,
+    ),
+)
 
 
 async def async_setup_entry(
@@ -28,48 +68,82 @@ async def async_setup_entry(
 ) -> None:
     """Electric Kiwi Sensor Setup."""
     api: ElectricKiwiApi = hass.data[DOMAIN][entry.entry_id]
+    account_api = ElectricKiwiAccountData(api, SCAN_INTERVAL)
+    await account_api.async_update()
 
-    async_add_entities([ElectricKiwiBalanceSensor(api)], True)
+    entities = [
+        ElectricKiwiAccountSensor(account_api, description)
+        for description in SENSOR_TYPES
+    ]
+    async_add_entities(entities)
 
 
-class ElectricKiwiBalanceSensor(SensorEntity):
+class ElectricKiwiAccountSensor(SensorEntity):
     """Entity object for Electric Kiwi sensor."""
 
-    def __init__(self, api: ElectricKiwiApi) -> None:
+    entity_description: ElectricKiwiSensorEntityDescription
+
+    def __init__(
+        self,
+        api: ElectricKiwiAccountData,
+        description: ElectricKiwiSensorEntityDescription,
+    ) -> None:
         """Entity object for Electric Kiwi sensor."""
-        self._api: ElectricKiwiApi = api
-        self._balance: AccountBalance = None
-        self._attr_native_unit_of_measurement = CURRENCY_DOLLAR
+        self._api: ElectricKiwiAccountData = api
+        self._balance: AccountBalance
+        self.entity_description = description
         self._attributes = {
             ATTR_ATTRIBUTION: ATTRIBUTION,
-            ATTR_FRIENDLY_NAME: FRIENDLY_NAME,
         }
-        self._attr_unique_id = f"{api.customer_number}_{api.connection_id}_balance"
+
+    @property
+    def unique_id(self) -> str:
+        """Return a unique ID."""
+        return f"{self._api.customer_number}_{self._api.connection_id}_{self.entity_description.key}"
 
     @property
     def name(self) -> str:
         """Return the name of the sensor."""
-        return FRIENDLY_NAME
+        return f"{self.entity_description.name}"
 
     @property
-    def native_value(self):
+    def native_value(self) -> str:
         """Return the state of the sensor."""
-        return self._balance.total_account_balance
+        return "self.entity_description.value_func(self._api.balance)"
 
     @property
-    def extra_state_attributes(self):
+    def extra_state_attributes(self) -> dict[str, str]:
         """Return the state attributes."""
         return self._attributes
 
-    @Throttle(SCAN_INTERVAL)
-    async def async_update(self):
+    async def async_update(self) -> None:
         """Get the Electric Kiwi Account Balance data from the web service."""
+        await self._api.throttled_update()
+
+        _LOGGER.debug("Account data from sensor: %s", self._api.balance)
+
+
+class ElectricKiwiAccountData:
+    """ElectricKiwi Data object."""
+
+    def __init__(self, api, interval) -> None:
+        """wrap the api so it can be shared across entities without making multiple calls."""
+        self._api = api
+        self.customer_number = api.customer_number
+        self.connection_id = api.connection_id
+        self.throttled_update = Throttle(interval)(self._async_update)
+        self.balance = AccountBalance
+        self.balance.total_account_balance = "0"
+        self.balance.total_running_balance = "0"
+
+    async def async_update(self) -> None:
+        """Async update to get the balances."""
+        await self._async_update()
+
+    async def _async_update(self) -> None:
         async with async_timeout.timeout(60):
             try:
-                self._balance = await self._api.get_account_balance()
-            except ApiException:
-                if self._balance is None:
-                    self._balance = AccountBalance
-                    self._balance.total_account_balance = "0"
-
-        _LOGGER.debug("Pricing data: %s", self._balance.total_account_balance)
+                self.balance = await self._api.get_account_balance()
+            except ApiException as err:
+                _LOGGER.error("Error updating account balance entities: %s", err)
+            _LOGGER.debug("Account data: %s", self.balance)
