48f5b89486fef25224083943282481c5f0447272
Michael Arthur <michael.arthur@propellerhead.co.nz>
2023-03-09 10:09:28 +1300

    move to using data coordinator pattern

--- homeassistant/components/electric_kiwi/sensor.py
+++ homeassistant/components/electric_kiwi/sensor.py
@@ -8,7 +8,7 @@ import logging
 
 import async_timeout
 from electrickiwi_api import ElectricKiwiApi
-from electrickiwi_api.exceptions import ApiException
+from electrickiwi_api.exceptions import ApiException, AuthException
 from electrickiwi_api.model import AccountBalance
 
 from homeassistant.components.sensor import (
@@ -20,8 +20,13 @@ from homeassistant.components.sensor import (
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import ATTR_ATTRIBUTION, CURRENCY_DOLLAR, PERCENTAGE
 from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ConfigEntryAuthFailed
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.util import Throttle
+from homeassistant.helpers.update_coordinator import (
+    CoordinatorEntity,
+    DataUpdateCoordinator,
+    UpdateFailed,
+)
 
 from .const import (
     ATTR_HOP_PERCENTAGE,
@@ -35,7 +40,7 @@ from .const import (
 
 _LOGGER = logging.getLogger(__name__)
 
-SCAN_INTERVAL = timedelta(hours=6)
+ACCOUNT_SCAN_INTERVAL = timedelta(hours=6)
 
 
 @dataclass
@@ -97,29 +102,33 @@ async def async_setup_entry(
     hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
 ) -> None:
     """Electric Kiwi Sensor Setup."""
-    api: ElectricKiwiApi = hass.data[DOMAIN][entry.entry_id]
-    account_api = ElectricKiwiAccountData(api, SCAN_INTERVAL)
-    await account_api.async_update()
+    ek_api: ElectricKiwiApi = hass.data[DOMAIN][entry.entry_id]
+    account_coordinator = ElectricKiwiAccountDataCoordinator(hass, ek_api)
+    await account_coordinator.async_config_entry_first_refresh()
 
     entities = [
-        ElectricKiwiAccountSensor(account_api, description)
+        ElectricKiwiAccountEntity(account_coordinator, description)
         for description in SENSOR_TYPES
     ]
     async_add_entities(entities)
 
 
-class ElectricKiwiAccountSensor(SensorEntity):
+class ElectricKiwiAccountEntity(CoordinatorEntity, SensorEntity):
     """Entity object for Electric Kiwi sensor."""
 
     entity_description: ElectricKiwiSensorEntityDescription
 
     def __init__(
         self,
-        api: ElectricKiwiAccountData,
+        account_coordinator: ElectricKiwiAccountDataCoordinator,
         description: ElectricKiwiSensorEntityDescription,
     ) -> None:
         """Entity object for Electric Kiwi sensor."""
-        self._api: ElectricKiwiAccountData = api
+        super().__init__(account_coordinator)
+        self._account_coordinator: ElectricKiwiAccountDataCoordinator = (
+            account_coordinator
+        )
+
         self._balance: AccountBalance
         self.entity_description = description
         self._attributes = {
@@ -129,7 +138,7 @@ class ElectricKiwiAccountSensor(SensorEntity):
     @property
     def unique_id(self) -> str:
         """Return a unique ID."""
-        return f"{self._api.customer_number}_{self._api.connection_id}_{self.entity_description.key}"
+        return f"{self._account_coordinator.customer_number}_{self._account_coordinator.connection_id}_{self.entity_description.key}"
 
     @property
     def name(self) -> str:
@@ -139,7 +148,7 @@ class ElectricKiwiAccountSensor(SensorEntity):
     @property
     def native_value(self) -> datetime | str | None:
         """Return the state of the sensor."""
-        return self.entity_description.value_func(self._api.balance)
+        return self.entity_description.value_func(self._account_coordinator.data)
 
     @property
     def extra_state_attributes(self) -> dict[str, str]:
@@ -147,30 +156,44 @@ class ElectricKiwiAccountSensor(SensorEntity):
         return self._attributes
 
     async def async_update(self) -> None:
-        """Get the Electric Kiwi Account Balance data from the web service."""
-        await self._api.throttled_update()
+        """Get the latest data from Electric Kiwi and updates the balances."""
+        await super().async_update()
+        _LOGGER.debug("Account data from sensor: %s", self._account_coordinator.data)
 
-        _LOGGER.debug("Account data from sensor: %s", self._api.balance)
 
-
-class ElectricKiwiAccountData:
+class ElectricKiwiAccountDataCoordinator(DataUpdateCoordinator):
     """ElectricKiwi Data object."""
 
-    def __init__(self, api, interval) -> None:
-        """wrap the api so it can be shared across entities without making multiple calls."""
-        self._api = api
-        self.customer_number = api.customer_number
-        self.connection_id = api.connection_id
-        self.throttled_update = Throttle(interval)(self.async_update)
-        self.balance = AccountBalance
-        self.balance.total_account_balance = "0"
-        self.balance.total_running_balance = "0"
+    def __init__(self, hass: HomeAssistant, ek_api: ElectricKiwiApi) -> None:
+        """Initialize ElectricKiwiAccountDataCoordinator."""
+        super().__init__(
+            hass,
+            _LOGGER,
+            # Name of the data. For logging purposes.
+            name="Electric Kiwi Account Data",
+            # Polling interval. Will only be polled if there are subscribers.
+            update_interval=ACCOUNT_SCAN_INTERVAL,
+        )
+        self._ek_api = ek_api
+        self.customer_number = ek_api.customer_number
+        self.connection_id = ek_api.connection_id
 
-    async def async_update(self) -> None:
-        """Async update to get the balances."""
-        async with async_timeout.timeout(60):
-            try:
-                self.balance = await self._api.get_account_balance()
-            except ApiException as err:
-                _LOGGER.error("Error updating account balance entities: %s", err)
-            _LOGGER.debug("Account data: %s", self.balance)
+    async def _async_update_data(self) -> AccountBalance:
+        """Fetch data from API endpoint.
+
+        This is the place to pre-process the data to lookup tables
+        so entities can quickly look up their data.
+        """
+        try:
+            # Note: asyncio.TimeoutError and aiohttp.ClientError are already
+            # handled by the data update coordinator.
+            async with async_timeout.timeout(60):
+                return await self._ek_api.get_account_balance()
+        except AuthException as auth_err:
+            # Raising ConfigEntryAuthFailed will cancel future updates
+            # and start a config flow with SOURCE_REAUTH (async_step_reauth)
+            raise ConfigEntryAuthFailed from auth_err
+        except ApiException as api_err:
+            raise UpdateFailed(
+                f"Error communicating with EK API: {api_err}"
+            ) from api_err
